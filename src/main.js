12335f007a5b28398710f8e107450353f87003e9
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2025-11-30 18:18:52 -0700

    testing the tailwind css in html and adjusting my files for coding

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1 class="text-4xl font-bold text-center text-red-600">
-    Tesla Clone
-  </h1>
-`
+console.log("Tesla Clone JS loaded");
